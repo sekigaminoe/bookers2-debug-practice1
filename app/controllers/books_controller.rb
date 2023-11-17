a1c26abffee18e12856964892aa0404ec9a9bbbb
EC2 Default User <ec2-user@ip-172-31-16-124.ap-northeast-1.compute.internal>
2023-11-17 07:53:53 +0000

    [Finish]閲覧数表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,9 @@ class BooksController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update, :destroy]
   def show
     @book = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end
     @book_new = Book.new
     @book_comment = BookComment.new
   end
